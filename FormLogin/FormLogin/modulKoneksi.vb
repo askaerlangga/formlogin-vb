3e66701f9048ee1f089565005ff653c6f2428a30
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-04-04 22:40:43 +0700

    Menambahkan modul koneksi

---
+++ FormLogin/FormLogin/modulKoneksi.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Data.Odbc
+Module modulKoneksi
+    Public CONN As OdbcConnection
+    Public CMD As OdbcCommand
+    Public DR As OdbcDataReader
+    Public DA As OdbcDataAdapter
+    Public DS As DataSet
+    Dim DB As String
+
+    Sub bukaKoneksi()
+        'Letak Database
+        DB = "Driver={MySQL ODBC 8.0 ANSI Driver};Database=mysql_crud;server=localhost;uid=root"
+        CONN = New OdbcConnection(DB)
+        If CONN.State = ConnectionState.Closed Then
+            CONN.Open()
+        End If
+    End Sub
+
+    Sub tutupKoneksi()
+        If CONN.State = ConnectionState.Open Then
+            CONN.Close()
+        End If
+    End Sub
+End Module
