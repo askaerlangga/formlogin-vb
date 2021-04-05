59c39c9ae5f17e3a8b99e6a56c92738dac12eabb
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-04-05 14:28:14 +0700

    Menambahkan formDashboard

---
+++ FormLogin/FormLogin/formDashboard.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class formDashboard
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnLogout = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnLogout
+        '
+        Me.btnLogout.Location = New System.Drawing.Point(413, 12)
+        Me.btnLogout.Name = "btnLogout"
+        Me.btnLogout.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogout.TabIndex = 0
+        Me.btnLogout.Text = "Logout"
+        Me.btnLogout.UseVisualStyleBackColor = True
+        '
+        'formDashboard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(500, 303)
+        Me.Controls.Add(Me.btnLogout)
+        Me.Name = "formDashboard"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Dashboard"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnLogout As Button
+End Class
---
+++ FormLogin/FormLogin/formDashboard.vb
@@ -0,0 +1,6 @@
+﻿Public Class formDashboard
+    Private Sub btnLogout_Click(sender As Object, e As EventArgs) Handles btnLogout.Click
+        Me.Close()
+        formLogin.Show()
+    End Sub
+End Class
